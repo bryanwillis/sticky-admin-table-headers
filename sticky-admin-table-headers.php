7da01ff921b6a650ffa547e49e542fc77339c545
Bryan Willis <willis.bryan@live.com>
2014-07-22 07:13:23 -0500

    Update sticky-admin-table-headers.php

--- sticky-admin-table-headers.php
+++ sticky-admin-table-headers.php
@@ -11,7 +11,6 @@ License: GPLv2 or later
 
 add_action( 'admin_enqueue_scripts', 'wp_admin_float_the_edit_screen_table_heads_enqueue_scripts', 20 );
 function wp_admin_float_the_edit_screen_table_heads_enqueue_scripts() {
-        
         wp_register_script( 'sticky-admin-table-headers', plugin_dir_url( __FILE__ ) . 'jquery.sticky-admin-table-headers.js', array( 'jquery' ), '1.0.0', true );		
         wp_register_script( 'sticky-admin-table-headers-init', plugin_dir_url( __FILE__ ) . 'jquery.sticky-admin-table-headers-init.js', array( 'sticky-admin-table-headers' ), '1.0.0', true );		
         wp_enqueue_script( 'sticky-admin-table-headers' );
