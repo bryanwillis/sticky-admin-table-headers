b64f6331d5cca6c3025b6abba92abc4ddee80706
Bryan Willis <willis.bryan@live.com>
2014-07-25 18:49:36 -0500

    Update sticky-admin-table-headers.php

--- sticky-admin-table-headers.php
+++ sticky-admin-table-headers.php
@@ -8,21 +8,16 @@ Author: Bryan Willis
 Author URI: http://profiles.wordpress.org/codecandid
 License: GPLv2 or later
 */
-
-add_action( 'admin_enqueue_scripts', 'wp_admin_edit_screen_table_sticky_header_enqueue_scripts', 20 );
-function wp_admin_edit_screen_table_sticky_header_enqueue_scripts() {
-        wp_register_script( 'sticky-admin-table-headers', plugin_dir_url( __FILE__ ) . 'jquery.sticky-admin-table-headers.js', array( 'jquery' ), '1.0.0', true );		
-        wp_enqueue_script( 'sticky-admin-table-headers' );
+if ( ! defined( 'WPINC' ) ) {
+	die;
 }
-
-
-add_action('admin_head-edit.php', 'wp_admin_edit_screen_table_sticky_header_css_admin_head');
-function wp_admin_edit_screen_table_sticky_header_css_admin_head() {
-	?>
-<style type="text/css">
-.tableFloatingHeaderOriginal {
-	background-color: #ffffff;
+if(!function_exists('wp_list_tables_sticky_headers_js')) {
+if( !is_admin() ) 
+	return;
+function wp_list_tables_sticky_headers_js( $hook_suffix ) {
+    if ( 'edit.php' !== $hook_suffix )
+        return;
+        wp_enqueue_script( 'sticky-admin-table-headers', plugin_dir_url( __FILE__ ) . 'jquery.sticky-admin-table-headers.js', array( 'jquery' ), '1.0.0', true );	
 }
-</style>
-	<?php
+	add_action( 'admin_enqueue_scripts', 'wp_list_tables_sticky_headers_js', 20 );
 }
